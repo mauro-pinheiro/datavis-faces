999220b793cd1e27d5959c449ba8d1c78346d158
mauro-pinheiro <mauro.pcsergio@gmail.com>
2020-10-09 18:51:21 -0300

    Varias Faces Com arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import './App.css';
 
+import { range } from 'd3';
 import { Face } from './components/Face';
 
-const width = 960;
-const height = 500;
+const width = 200;
+const height = 200;
+
+const array = range(9*3);
 
 const App = () => (
-  <Face
-    width={width}
-    height={height}
-    centerX={width / 2}
-    centerY={height / 2}
-    eyeRadius={50}
-    strokeWidth={10}
-    eyeOffsetX={90}
-    eyeOffsetY={100}
-    mouthWidth={20}
-    mouthRadius={140}
-  />
-)
+  <>
+    {array.map((value: number) => (
+    <Face
+      key={value}
+      width={width}
+      height={height}
+      centerX={width / 2}
+      centerY={height / 2}
+      eyeRadius={10}
+      strokeWidth={10}
+      eyeOffsetX={30}
+      eyeOffsetY={30}
+      mouthWidth={10}
+      mouthRadius={40}
+    />
+  ))}
+  </>
+);
 
 export default App;
