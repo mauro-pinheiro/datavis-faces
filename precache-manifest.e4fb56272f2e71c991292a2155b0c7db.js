76c19a87e7f46fe6ce8cc6ece699507acbf5a26f
mauro-pinheiro <mauro.pcsergio@gmail.com>
2020-10-09 23:10:24 +0000

    Updates

---
+++ precache-manifest.e4fb56272f2e71c991292a2155b0c7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ac0954e11c670029591bde8d22c366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d1d1a804add533de170",
+    "url": "/static/css/main.526deb67.chunk.css"
+  },
+  {
+    "revision": "65e455ee5231a9fa7bb2",
+    "url": "/static/js/2.12267877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12267877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1d1a804add533de170",
+    "url": "/static/js/main.cc1a069f.chunk.js"
+  },
+  {
+    "revision": "e91398ec253ad8957855",
+    "url": "/static/js/runtime-main.8d771be7.js"
+  }
+]);
